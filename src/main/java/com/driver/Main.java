957786faeb492e557ab83761eedd86200d05d4c4
AbhijeetKambare <abhijeetkambare1999@gmail.com>
2023-01-13 20:56:45 +0530

    made cahnges

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        String name="sai";
+        RWOnly rw=new RWOnly(name);
+//        int a=10;
+//        rw.variable=a;
+//        //'variable' has private access in 'com.driver.RWOnly'
+        System.out.println(rw.setName(name));
+    }
+
 }
