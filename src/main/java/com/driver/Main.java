c49875d65f84a5dcc39c89739d8cb472ab541ed9
AbhijeetKambare <abhijeetkambare1999@gmail.com>
2023-01-17 02:20:23 +0530

    solution

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,11 +3,12 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         String name="sai";
-        RWOnly rw=new RWOnly(name);
+        RWOnly rw=new RWOnly();
 //        int a=10;
 //        rw.variable=a;
 //        //'variable' has private access in 'com.driver.RWOnly'
-        System.out.println(rw.setName(name));
+        rw.setName(name);
+        rw.getName();
     }
 
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,17 +1,14 @@
 package com.driver;
 
 public class RWOnly {
- private String name="abhi";
- public RWOnly(String name){
-  this.name=name;
- }
+ private String name;
+
  public String getName() {
   return name;
  }
 
- public String setName(String name) {
+ public void setName(String name) {
   this.name = name;
-  return name;
  }
 }
 
